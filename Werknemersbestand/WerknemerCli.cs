cac40b0fda85d110e8a829a90831cfefbec3a0da
Filip Geens <filipg@waegener.com>
2020-03-10 22:00:52 +0100

    Werknemersbestand

---
+++ Werknemersbestand/WerknemerCli.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Werknemersbestand {
+	public class WerknemerCli {
+		Werknemersbestand werknemers = new Werknemersbestand();
+		public virtual bool Execute(CLIBase parent, CliCommand Input) {
+			switch (Input.Command) {
+				case "add-werknemer":
+					werknemers.Add(Input[0], Input[1]);
+					return true;
+				case "zoek-werknemer":
+					var res=werknemers.ZoekOpNaam(Input[0]);
+					foreach (Werknemer item in res) {
+						Console.WriteLine(item);
+					}
+					return true;
+			}
+			return false;
+		}
+
+	}
+}
